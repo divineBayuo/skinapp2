916f33b8e21c68d46b94270bd15e8bcddadc9482
Divine Bayuo <divinebayuo@Divines-MacBook-Pro.local>
2026-04-21 12:09:57 +0000

    Setup firebase, firestore for skin-ntd

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -37,6 +40,11 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    // Google services plugin reads google-services.json
+    implementation platform('com.google.firebase:firebase-bom:33.0.0')
 }
 
 flutter {
